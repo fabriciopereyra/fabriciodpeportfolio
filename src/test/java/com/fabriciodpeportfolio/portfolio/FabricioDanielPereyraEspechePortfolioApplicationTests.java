b92eb3c078d83d55f156816965961002d9d91f30
PEREYRA ESPECHE, Fabricio Daniel <fabriciopereyra@gmail.com>
2023-06-16 21:21:17 -0300

    Iniciando GIT - Conectando Base de Datos

---
+++ src/test/java/com/fabriciodpeportfolio/portfolio/FabricioDanielPereyraEspechePortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabriciodpeportfolio.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricioDanielPereyraEspechePortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
