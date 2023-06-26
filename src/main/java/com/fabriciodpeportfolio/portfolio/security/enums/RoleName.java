bd44524127fbac85d30502ae71872f72b302ce70
PEREYRA ESPECHE, Fabricio Daniel <fabriciopereyra@gmail.com>
2023-06-26 20:08:56 -0300

    Creando roles en RoleName,java

---
+++ src/main/java/com/fabriciodpeportfolio/portfolio/security/enums/RoleName.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fabriciodpeportfolio.portfolio.security.enums;
+
+/**
+ *
+ * @author Fabricio
+ */
+public enum RoleName {
+
+    ROLE_ADMIN, ROLE_USER;
+    
+}
