08a3536cd48bbde7583bb571ac4efc35d34808a3
PEREYRA ESPECHE, Fabricio Daniel <fabriciopereyra@gmail.com>
2023-06-26 21:52:54 -0300

    Creando dto de login usuario en UserLogin.java

---
+++ src/main/java/com/fabriciodpeportfolio/portfolio/security/dto/UserLogin.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fabriciodpeportfolio.portfolio.security.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+/**
+ *
+ * @author Fabricio
+ */
+public class UserLogin {
+
+    @NotBlank
+    private String userName;
+
+    @NotBlank
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
