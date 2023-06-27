f0c1c069ee5bca35d42f129121aa56ea7432c6fd
PEREYRA ESPECHE, Fabricio Daniel <fabriciopereyra@gmail.com>
2023-06-26 21:54:14 -0300

    Creando mensaje en el controlador en Message.java

---
+++ src/main/java/com/fabriciodpeportfolio/portfolio/security/controller/Message.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fabriciodpeportfolio.portfolio.security.controller;
+
+/**
+ *
+ * @author Fabricio
+ */
+public class Message {
+
+    private String message;
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
